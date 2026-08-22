92ca71bd4aa5794e95a6e4c8c1f85238d4b8c2cb
HOON\hoonc <y88.choi@gmail.com>
2026-08-22 21:37:33 +0900

    파이날 버전

--- main.py
+++ main.py
@@ -23,17 +23,24 @@ for df in [train, test]:
     df["Age"] = df["Age"].fillna(df["Age"].median())
 
     df["Group"] = df["PassengerId"].str.split("_").str[0]
+    df["GroupSize"] = df.groupby("Group")["Group"].transform("count")
     df["HomePlanet"] = df["HomePlanet"].fillna(df.groupby("Group")["HomePlanet"].transform("first"))
     df["HomePlanet"] = df["HomePlanet"].fillna("Unknown")
     df["Destination"] = df["Destination"].fillna(df.groupby("Group")["Destination"].transform("first"))
     df["Destination"] = df["Destination"].fillna("Unknown")
-    # df["Cabin"] = df["Cabin"].fillna(df.groupby("Group")["Cabin"].transform("first"))
 
-
-
+    df[["Deck", "Num", "Side"]] = df["Cabin"].str.split("/", expand=True)
+    df["Deck"] = df["Deck"].fillna(
+        df.groupby("Group")["Deck"].transform(lambda s: s.mode()[0] if s.notna().any() else None)
+    )
+    df["Side"] = df["Side"].fillna(
+        df.groupby("Group")["Side"].transform(lambda s: s.mode()[0] if s.notna().any() else None)
+    )
+    df["Deck"] = df["Deck"].fillna(df["Deck"].mode()[0])
+    df["Side"] = df["Side"].fillna(df["Side"].mode()[0])
 # df에서 특징 고르기
-num_features = ["CryoSleep", "Age"] + spend_cols
-cat_features = ["HomePlanet", "Destination"]
+num_features = ["CryoSleep", "Age", "GroupSize"] + spend_cols
+cat_features = ["HomePlanet", "Destination", "Deck", "Side"]
 
 x = pd.get_dummies(train[num_features + cat_features], columns=cat_features)
 x_test = pd.get_dummies(test[num_features + cat_features], columns=cat_features)
@@ -58,13 +65,13 @@ test[["PassengerId", "Transported"]].to_csv("data/result.csv", index=False)
 
 # print(train.info())
 # print(test.info())
-print(train.isna().sum())
-print()
-print(test.isna().sum())
-
-x_tr, x_val, y_tr, y_val = train_test_split(x, y, test_size=0.2, random_state=42)
-
-for d in [3, 5, 8, 12, None]:
-    m2 = RandomForestClassifier(n_estimators=100, random_state=42, max_depth=d)
-    m2.fit(x_tr, y_tr)
-    # print(f"max_depth={str(d):>4} → {m2.score(x_val, y_val):.4f}")
+# print(train.isna().sum())
+# print()
+# print(test.isna().sum())
+#
+# x_tr, x_val, y_tr, y_val = train_test_split(x, y, test_size=0.2, random_state=42)
+#
+# for d in [3, 5, 8, 12, None]:
+#     m2 = RandomForestClassifier(n_estimators=100, random_state=42, max_depth=d)
+#     m2.fit(x_tr, y_tr)
+#     print(f"max_depth={str(d):>4} → {m2.score(x_val, y_val):.4f}")
